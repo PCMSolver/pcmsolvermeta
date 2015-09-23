f21b65e3f0d071e75a6a0acf1b63be652242346b
Roberto Di Remigio <roberto.d.remigio@uit.no>
2015-09-23 11:20:01 +0200

    Add dashboard scripts

---
+++ merzbow/RDR-clang3.5-release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source /Users/roberto/.bashrc
+export PATH=/opt/intel/composer_xe_2015.0.077/bin/intel64:/opt/intel/composer_xe_2015.0.077/mpirt/bin/intel64:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/opt/intel/composer_xe_2015.0.077/debugger/gdb/intel64/bin
+
+PCMSolver_TMPDIR=/Users/roberto/Scratch/RDR-clang3.5-release
+mkdir -p $PCMSolver_TMPDIR
+export PCMSolver_TMPDIR
+export NPROCS=`sysctl -n hw.ncpu`
+export CTEST_MAKE_NUM_PROCS=$NPROCS
+
+TMP_DIR=/Users/roberto/Scratch/tmprunpcmsolver/RDR-clang3.5-release
+mkdir -p $TMP_DIR
+
+git clone git@gitlab.com:PCMSolver/pcmsolver.git $TMP_DIR
+
+cd $TMP_DIR
+
+./setup --fc=gfortran --cc=clang --cxx=clang++ --type=release -D BUILDNAME=RDR-MacOS-10.10.4-clang3.5-release -D SITE=merzbow --python=/usr/local/bin/python
+
+cd $TMP_DIR/objdir
+
+export PCMSolverDATADIR=$TMP_DIR/lib
+mkdir -p $PCMSolver_TMPDIR/pcmsolverscr
+export PCMSolver_SCRATCH=$PCMSolver_TMPDIR/pcmsolverscr
+
+ctest -D Nightly -j$NPROCS
+
+cd
+rm -rf $PCMSolver_TMPDIR $TMP_DIR
+
+exit 0
